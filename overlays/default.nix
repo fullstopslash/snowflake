3439fa4a5132af103aecea4fedb9366717761452
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-08-05 20:07:07 -0600

    refactor: overlay for unstable.mesa

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,6 +20,7 @@ let
     # example = prev.example.overrideAttrs (oldAttrs: let ... in {
     # ...
     # });
+
     #    flameshot = prev.flameshot.overrideAttrs {
     #      cmakeFlags = [
     #        (prev.lib.cmakeBool "USE_WAYLAND_GRIM" true)
@@ -41,8 +42,31 @@ let
     unstable = import inputs.nixpkgs-unstable {
       inherit (final) system;
       config.allowUnfree = true;
-      #      overlays = [
-      #     ];
+      overlays = [
+        (final: prev: {
+          mesa = prev.mesa.overrideAttrs (
+            _:
+            let
+              version = "25.1.6";
+              hashes = {
+                "25.1.6" = "sha256-shyyezt2ez9awviatec6wvmzmujusoyxxlugs1q6q7u=";
+                "25.1.5" = "sha256-azad1/wiz8d0lxpim9obp6/k7ysp12rgfe8jzrc9gl0=";
+                "25.1.4" = "sha256-DA6fE+Ns91z146KbGlQldqkJlvGAxhzNdcmdIO0lHK8=";
+              };
+            in
+            rec {
+              inherit version;
+              src = _prev.fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "mesa";
+                repo = "mesa";
+                rev = "mesa-${version}";
+                sha256 = if hashes ? ${version} then hashes.${version} else "";
+              };
+            }
+          );
+        })
+      ];
     };
   };
 
