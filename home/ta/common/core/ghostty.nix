b741fd63bbe635f658fa07151b6bd79b71b67f46
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-08-15 08:19:09 -0600

    refactor: clean up from lazy copy/paste

--- home/ta/common/core/ghostty.nix
+++ home/ta/common/core/ghostty.nix
@@ -1,15 +1,7 @@
-{ pkgs, lib, ... }:
+{ ... }:
 {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty.overrideAttrs (_: {
-      # https://github.com/NixOS/nixpkgs/issues/421442
-      preBuild = lib.optionalString (lib.versionOlder pkgs.unstable.linux.version "6.15.5") ''
-        shopt -s globstar
-        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-        shopt -u globstar
-      '';
-    });
     settings = {
       scrollback-limit = 10000;
     };
