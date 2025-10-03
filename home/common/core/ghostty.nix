29c6de2826bf78869dd1f8f4d30f3111251239a6
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-03 10:27:36 -0600

    refactor: disable most ghostty binds

--- home/common/core/ghostty.nix
+++ home/common/core/ghostty.nix
@@ -2,8 +2,17 @@
 {
   programs.ghostty = {
     enable = true;
+    clearDefaultKeybinds = true;
     settings = {
       scrollback-limit = 10000;
+      #NOTE(ghostty): not using ghostty for splits or tabs so nearly all default binds conflict Hypr, nvim, or zellij
+      keybind = [
+        "ctrl+shift+d=inspector:toggle"
+        "ctrl+shift+c=copy_to_clipboard"
+        "ctrl+shift+v=paste_from_clipboard"
+        # Fix fixterm conflict with zsh ^[ character https://github.com/ghostty-org/ghostty/discussions/5071
+        "ctrl+left_bracket=text:\\x1b"
+      ];
     };
   };
 }
