e40bce778bef10fd901c4e667529f82693970057
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-04 22:46:54 -0600

    refactor: ghostty unbinds redo... less scorched earthy

--- home/common/core/ghostty.nix
+++ home/common/core/ghostty.nix
@@ -2,7 +2,6 @@
 {
   programs.ghostty = {
     enable = true;
-    clearDefaultKeybinds = true;
     settings = {
       scrollback-limit = 10000;
       #NOTE(ghostty): not using ghostty for splits or tabs so nearly all default binds conflict Hypr, nvim, or zellij
@@ -12,6 +11,15 @@
         "ctrl+shift+v=paste_from_clipboard"
         # Fix fixterm conflict with zsh ^[ character https://github.com/ghostty-org/ghostty/discussions/5071
         "ctrl+left_bracket=text:\\x1b"
+        "ctrl+shift+minus=decrease_font_size:1"
+        "ctrl+shift+plus=increase_font_size:1"
+        "ctrl+shift+0=reset_font_size"
+        #
+        # ========== UNBIND ==========
+        #
+        "ctrl+shift+e=unbind" # new_split
+        "ctrl+shift+n=unbind" # new_window
+        "ctrl+shift+t=unbind" # new_tab
       ];
     };
   };
