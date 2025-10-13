9482a31e522261cb37f424bf4f49b75ea0708c55
Daniel Wilcox <danielwilcoxjr@gmail.com>
2025-10-13 17:31:45 -0500

    feat: Add Hyprland cachix for pre-built binaries
    Adds hyprland.cachix.org to substituters to avoid building Hyprland from source.

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -36,12 +36,14 @@
       substituters = [
         "https://cache.nixos.org" # Official global cache
         "https://nix-community.cachix.org" # Community packages
+        "https://hyprland.cachix.org" # Hyprland cache
       ];
       #extra-substituters = [
       #  "https://nix-community.cachix.org" # Nix community Cachix server
       #];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" # Hyprland cache
       ];
     };
 
