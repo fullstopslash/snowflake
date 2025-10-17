f28d75e68ea26f82186821624a6e78c6900e935a
Daniel Wilcox <danielwilcoxjr@gmail.com>
2025-10-17 16:03:55 -0500

    feat(sddm): add pkgs.sddm-astronaut and set theme to sddm-astronaut-theme

--- hosts/common/optional/services/sddm.nix
+++ hosts/common/optional/services/sddm.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 {
+  environment.systemPackages = [ pkgs.sddm-astronaut ];
+
   services.displayManager.sddm = {
     enable = true;
-    wayland = {
-      enable = true;
-      compositor = "kwin";
-    };
+    enableHidpi = true;
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
     settings = {
-      Theme = {
-        Current = "breeze";
+      General = {
+        DisplayServer = "wayland";
+        GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2 QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
+      };
+      Wayland = {
+        CompositorCommand = "Hyprland --no-lockscreen --no-global-shortcuts";
       };
     };
   };
