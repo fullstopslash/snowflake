955404ba25e443e673ca1b11f06b4c42f912ed7f
Daniel Wilcox <danielwilcoxjr@gmail.com>
2025-10-18 00:49:38 -0500

    feat(qtgreet): set background via /etc/qtgreet/config.ini; set avatar via AccountsService links

--- hosts/common/optional/services/greetd.nix
+++ hosts/common/optional/services/greetd.nix
@@ -27,6 +27,21 @@ in
 
   config = {
     environment.systemPackages = [ pkgs.greetd.qtgreet pkgs.cage ];
+
+    # qtgreet appearance
+    services.accounts-daemon.enable = true;
+    environment.etc."qtgreet/config.ini".text = ''
+      [Appearance]
+      Background=/etc/qtgreet/background
+      BaseColor=ff000000
+      TextColor=ffffffff
+    '';
+    # Link background and avatar from user-provided paths
+    systemd.tmpfiles.rules = [
+      "L /etc/qtgreet/background - - - - /home/rain/Wallpapers/cool/wallhaven-qr2qx5.fav2.webp"
+      "L /var/lib/AccountsService/icons/${config.hostSpec.username} - - - - /home/rain/PirateSoftwareFlat.svg"
+      "L /var/lib/AccountsService/users/${config.hostSpec.username} - - - - ${pkgs.writeText "acc-user-${config.hostSpec.username}" ''[User]\nIcon=/var/lib/AccountsService/icons/${config.hostSpec.username}\n''}"
+    ];
     services.greetd = {
       enable = true;
 
